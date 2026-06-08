c0d8f5367a6eb98d9e126197ca8b20c3413ce388
jeff.snyder <jeff.snyder@bgi-llc.com>
2026-06-08 14:32:07 -0700

    Run program in main

--- main.cpp
+++ main.cpp
@@ -1,37 +1,17 @@
 #include <iostream>
 #include <fstream>
-#include <string>
-#include <vector>
-
-enum class Token {
-	open_brace,
-	close_brace,
-	open_paren,
-	close_paren,
-	semicolon,
-	keyword,
-	identifier, // all non-keyword words
-	integer_literal, // all integers
-};
+#include "generate_assembly.hpp"
 
 int main(int argc, char* argv[]) {
-	std::vector<Token> tokens;
-
 	std::fstream file("inputs/return_2.c");
+	std::string file_contents((std::istreambuf_iterator<char>(file)),
+							   std::istreambuf_iterator<char>());
+	auto tokens = tokenize(file_contents);
+	Parser parser{ tokens };
+	auto ast = parser.parse();
 
-	char ch;
-	while (file >> ch) {
-		switch (ch) {
-		case '{':
-			tokens.push_back(Token::open_brace);
-			break;
-		default:
-			if (std::isalpha(ch)) {
-			}
-			else if (std::isdigit(ch)) {
-			}
-		}
-	}
+	generateAssembly(ast.get());
+
 	std::cin.get();
 	return 0;
 }
