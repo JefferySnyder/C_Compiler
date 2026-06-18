59b58cf319ebee67ddf5e4e0c4fc086a5089a43a
jeff.snyder <jeff.snyder@bgi-llc.com>
2026-06-18 12:31:40 -0700

    Add the ability to parse loops

--- parser.hpp
+++ parser.hpp
@@ -2,7 +2,7 @@
 #include <functional>
 #include "lexer.hpp"
 
-enum class ASTNodeType { Function, ReturnStmt, Declaration, ExprStmt, ConditionalStmt, CompoundStmt, Literal, Unary, Binary, Assign, Var, CompoundAssign, ConditionalExpr };
+enum class ASTNodeType { Function, ReturnStmt, Declaration, ExprStmt, ConditionalStmt, CompoundStmt, ForStmt, WhileStmt, DoStmt, BreakStmt, ContinueStmt, Literal, Unary, Binary, Assign, Var, CompoundAssign, ConditionalExpr };
 
 struct ASTNode { 
 	ASTNodeType type;
@@ -123,6 +123,46 @@ struct CompoundStmtNode : public ASTNode {
 	};
 };
 
+struct ForStmtNode : public ASTNode {
+	std::unique_ptr<ASTNode> init;
+	std::unique_ptr<ASTNode> cond;
+	std::unique_ptr<ASTNode> post;
+	std::unique_ptr<ASTNode> body;
+	ForStmtNode(std::unique_ptr<ASTNode> i, std::unique_ptr<ASTNode> c, std::unique_ptr<ASTNode> p, std::unique_ptr<ASTNode> b)
+		:init{ std::move(i) }, cond{ std::move(c) }, post{ std::move(p) }, body{ std::move(b) }
+	{
+		type = ASTNodeType::ForStmt;
+	};
+};
+
+struct WhileStmtNode : public ASTNode {
+	std::unique_ptr<ASTNode> init;
+	std::unique_ptr<ASTNode> body;
+	WhileStmtNode(std::unique_ptr<ASTNode> i, std::unique_ptr<ASTNode> b)
+		:init{ std::move(i) }, body{ std::move(b) }
+	{
+		type = ASTNodeType::WhileStmt;
+	};
+};
+
+struct DoStmtNode : public ASTNode {
+	std::unique_ptr<ASTNode> body;
+	std::unique_ptr<ASTNode> init;
+	DoStmtNode(std::unique_ptr<ASTNode> b, std::unique_ptr<ASTNode> i)
+		:body{ std::move(b) }, init{ std::move(i) }
+	{
+		type = ASTNodeType::DoStmt;
+	};
+};
+
+struct BreakStmtNode : public ASTNode {
+	BreakStmtNode() { type = ASTNodeType::BreakStmt; };
+};
+
+struct ContinueStmtNode : public ASTNode {
+	ContinueStmtNode() { type = ASTNodeType::ContinueStmt; };
+};
+
 struct FunctionNode : public ASTNode {
 	std::string name;
 	std::unique_ptr<ASTNode> body;
@@ -172,9 +212,9 @@ private:
 		return res;
 	}
 	std::unique_ptr<ASTNode> parse_declaration() {
-		std::unique_ptr<ASTNode> res;
 		ensure(TokenType::Int);
 		auto var_name = ensure(TokenType::Identifier);
+		std::unique_ptr<ASTNode> res;
 		if (tokens[index].type == TokenType::Assignment) {
 			ensure(TokenType::Assignment);
 			auto exp = parse_expr();
@@ -212,17 +252,68 @@ private:
 		else if (tokens[index].type == TokenType::OpenBrace) {
 			res = parse_block();
 		}
-		else {
+		else if (tokens[index].type == TokenType::For) {
+			ensure(TokenType::For);
+			ensure(TokenType::OpenParen);
+			std::unique_ptr<ASTNode> init;
+			if (tokens[index].type == TokenType::Int) {
+				init = parse_declaration();
+			}
+			else {
+				init = parse_opt_expr();
+				ensure(TokenType::Semicolon);
+			}
+			auto cond = parse_opt_expr();
+			if (cond == nullptr) {
+				cond = std::make_unique<LiteralNode>(1);
+			}
+			ensure(TokenType::Semicolon);
+			auto post = parse_opt_expr();
+			ensure(TokenType::CloseParen);
+			auto stmt = parse_stmt();
+			res = std::make_unique<ForStmtNode>(std::move(init), std::move(cond), std::move(post), std::move(stmt));
+		}
+		else if (tokens[index].type == TokenType::While) {
+			ensure(TokenType::While);
+			ensure(TokenType::OpenParen);
 			auto exp = parse_expr();
-			res = std::make_unique<ExprStmtNode>(std::move(exp));
+			ensure(TokenType::CloseParen);
+			auto stmt = parse_stmt();
+			res = std::make_unique<WhileStmtNode>(std::move(exp), std::move(stmt));
+		}
+		else if (tokens[index].type == TokenType::Do) {
+			ensure(TokenType::Do);
+			auto stmt = parse_stmt();
+			ensure(TokenType::While);
+			ensure(TokenType::OpenParen);
+			auto exp = parse_expr();
+			ensure(TokenType::CloseParen);
+			res = std::make_unique<DoStmtNode>(std::move(stmt), std::move(exp));
+			ensure(TokenType::Semicolon);
+		}
+		else if (tokens[index].type == TokenType::Break) {
+			ensure(TokenType::Break);
+			ensure(TokenType::Semicolon);
+		}
+		else if (tokens[index].type == TokenType::Continue) {
+			ensure(TokenType::Continue);
+			ensure(TokenType::Semicolon);
+		}
+		else {
+			res = std::make_unique<ExprStmtNode>(parse_opt_expr());
 			ensure(TokenType::Semicolon);
 		}
 		return res;
 	}
+	std::unique_ptr<ASTNode> parse_opt_expr() {
+		if (tokens[index].type == TokenType::Semicolon || tokens[index].type == TokenType::CloseParen)
+			return nullptr;
+		return parse_expr();
+	}
 	std::unique_ptr<ASTNode> parse_expr() {
 		auto id = parse_assign_expr();
 		std::unordered_set<TokenType> caops{TokenType::PlusEquals, TokenType::MinusEquals};
-		if (id->type == ASTNodeType::Var && caops.contains(tokens[index].type)) {
+		if (caops.contains(tokens[index].type) && id->type == ASTNodeType::Var) {
 			auto tt = tokens[index++].type;
 			auto right = parse_expr();
 			id = std::make_unique<CompoundAssignNode>(std::move(id), tt, std::move(right));
@@ -231,7 +322,7 @@ private:
 	}
 	std::unique_ptr<ASTNode> parse_assign_expr() {
 		auto id = parse_conditional_expr();
-		if (id->type == ASTNodeType::Var && tokens[index].type == TokenType::Assignment) {
+		if (tokens[index].type == TokenType::Assignment && id->type == ASTNodeType::Var) {
 			ensure(TokenType::Assignment);
 			auto right = parse_expr();
 			id = std::make_unique<AssignNode>(std::move(id), std::move(right));
@@ -308,23 +399,27 @@ private:
 		return left;
 	}
 	std::unique_ptr<ASTNode> parse_factor() {
-		auto next_tok = tokens[index++];
-		if (next_tok.type == TokenType::OpenParen) {
+		auto peek_tok_type = tokens[index].type;
+		if (peek_tok_type == TokenType::OpenParen) {
+			ensure(TokenType::OpenParen);
 			auto res = parse_expr();
 			ensure(TokenType::CloseParen);
 			return res;
 		}
 		std::unordered_set<TokenType> un_ops{TokenType::Bang, TokenType::Tilde, TokenType::Minus};
-		if (un_ops.contains(next_tok.type)) {
-			auto un_op = next_tok.type;
+		if (un_ops.contains(peek_tok_type)) {
+			auto un_op = peek_tok_type;
+			ensure(peek_tok_type);
 			auto expr = parse_factor();
 			return std::make_unique<UnaryNode>(un_op, std::move(expr));
 		}
-		if (next_tok.type == TokenType::IntegerLiteral) {
-			return std::make_unique<LiteralNode>(std::stoi(next_tok.value));
+		if (peek_tok_type == TokenType::IntegerLiteral) {
+			auto value = ensure(TokenType::IntegerLiteral);
+			return std::make_unique<LiteralNode>(std::stoi(value));
 		}
-		if (next_tok.type == TokenType::Identifier) {
-			return std::make_unique<VarNode>(next_tok.value);
+		if (peek_tok_type == TokenType::Identifier) {
+			auto value = ensure(TokenType::Identifier);
+			return std::make_unique<VarNode>(value);
 		}
 		fail();
 		return nullptr;
