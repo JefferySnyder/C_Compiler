8f57730199a9f403fb695af7536b3f90cca14330
jeff.snyder <jeff.snyder@bgi-llc.com>
2026-06-26 09:41:58 -0700

    Update context class to add variable type

--- context.hpp
+++ context.hpp
@@ -1,8 +1,14 @@
 #include <unordered_map>
 #include <string>
 
+struct Info {
+	TokenType type;
+	int stack_index;
+	Info(TokenType t, int si) : type{ t }, stack_index{ si } {};
+};
+
 struct Context {
-	std::unordered_map<std::string, int> var_map;
+	std::unordered_map<std::string, Info> var_map;
 	int stack_index = 0;
 	std::string break_label;
 	std::string continue_label;
